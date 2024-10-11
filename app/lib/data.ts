7582de44312e624804c8e6bf6339cdae8fc6156f
pockiiie <pockiemail@gmail.com>
2024-10-11 21:22:53 +0700

    chapter 11

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -15,7 +15,7 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -30,6 +30,8 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -49,6 +51,9 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
+
+  await new Promise((resolve) => setTimeout(resolve, 4000));
+
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
