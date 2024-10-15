f63fea5f31a918103fd6f4208a5291a1e4c77d1b
pockiiie <pockiemail@gmail.com>
2024-10-15 14:15:10 +0700

    Update Invoice error handling

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -11,7 +11,6 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   const handleSearch = useDebouncedCallback((term) => {
-    console.log(`Searcing ... ${term}`);
 
     const params = new URLSearchParams(searchParams);
     params.set('page', '1');
